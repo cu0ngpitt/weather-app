4cd40a11a27a54ce6cee25bead2124303643b18d
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-12-28 02:18:06 -0600

    Component and Http module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MainWeatherComponent } from './components/main-weather/main-weather.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
