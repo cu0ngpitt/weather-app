358d5f0f5d5aa70fbcf83c6d6a71d701410cc451
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-06 22:11:46 -0600

    Route for weather api call

--- routes/darksky.js
+++ routes/darksky.js
@@ -5,15 +5,28 @@ const router = express.Router();
 
 router.get('/data', (req, res, next) => {
 
-  Request.get(APIUrl + "/32.966904,-96.959011?units=us&exclude=minutely,daily,hourly,flags", (err, response, data) => {
+  Request.get(APIUrl + "/32.966904,-96.959011?units=us&exclude=minutely,hourly,flags", (error, response, data) => {
     let info = JSON.parse(data);
 
-    if(err) {
-      return console.dir(err);
+    if(error) {
+      return console.dir(error);
     } else if(data) {
       res.json(info);
     }
   });
 })
+
+// router.get('/imperial', (req, res, next) => {
+//
+//   Request.get(APIUrl + "/32.966904,-96.959011?units=us&exclude=flags", (error, response, data) => {
+//     let info = JSON.parse(data);
+//
+//     if(error) {
+//       return console.dir(error);
+//     } else if(data) {
+//       res.json(info);
+//     }
+//   });
+// })
 
 module.exports = router;
