e124546fe1980b7353e1e741fc640eac029f5764
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-10 14:46:36 -0600

    Refactored weather service for new api

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { HighLows } from '../../assets/models/highlows';
-
 
 @Injectable({
   providedIn: 'root'
@@ -11,37 +9,13 @@ export class WeatherService {
 
   public units: boolean;
 
-  private metricWeatherUrl = 'http://localhost:3000/weather/metric';
-  private imperialWeatherUrl = 'http://localhost:3000/weather/imperial';
-
+  private weatherUrl = 'http://localhost:3000/weather/data';
 
   constructor(private http: HttpClient) {
   }
 
-  getWeatherMetric() {
+  getWeather() {
     return this.http.get(this.weatherUrl);
   }
-
-  // getWeatherImperial() {
-  //   return this.http.get(this.weatherUrl + 'q=Coppell,us&units=imperial&APPID=' + this.apiKey);
-  // }
-  //
-  // getForecastMetric() {
-  //   return this.http.get(this.forecastUrl + 'q=Coppell,us&units=metric&APPID=' + this.apiKey)
-  //     .subscribe((data: any) => {
-  //       this.forecastData = data.list;
-  //       this.findForecastData();
-  //     });
-  // }
-  //
-  // getForecastImperial() {
-  //   return this.http.get(this.forecastUrl + 'q=Coppell,us&units=imperial&APPID=' + this.apiKey)
-  //     .subscribe((data: any) => {
-  //       this.forecastData = data.list;
-  //       this.findForecastData();
-  //     });
-  // }
-
-
 
 }
