8958d7a4a32d134dd67cfa0cde9c90e00ed29637
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-10 14:45:45 -0600

    Basic routing setup for api calls
    Will need to change later when I take in user location input

--- routes/darksky.js
+++ routes/darksky.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/data', (req, res, next) => {
 
-  Request.get(APIUrl + "/32.966904,-96.959011?units=us&exclude=minutely,hourly,flags", (error, response, data) => {
+  Request.get(APIUrl + "/32.966904,-96.959011?units=us&exclude=flags", (error, response, data) => {
     let info = JSON.parse(data);
 
     if(error) {
