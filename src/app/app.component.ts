bb00cd17711d746ebeeb20c4acd7ce8b5b5f7022
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-18 14:09:06 -0600

    Temporarily adding the units selection logic for functionality testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { WeatherService } from './services/weather.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+
+  constructor(public weather: WeatherService) {
+  }
+
+  toggleMetric() {
+    this.weather.units = "metric";
+  }
+
+  toggleImperial() {
+    this.weather.units = "imperial";
+    
+  }
+
 }
