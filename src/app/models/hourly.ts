53bd92d1ed166d29d7fea26fcd820ae9ceef8563
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-18 14:05:09 -0600

    Created data interfaces

---
+++ src/app/models/hourly.ts
@@ -0,0 +1,22 @@
+export interface Hourly {
+  apparentTemperature: number;
+  cardinal: string;
+  cloudCover: number;
+  dewPoint: number;
+  humidity: number;
+  icon: string;
+  ozone: number;
+  precipIntensity: number;
+  precipProbability: number;
+  pressure: number;
+  summary: string;
+  tempC: number;
+  temperature: number;
+  time: number;
+  uvIndex: number;
+  visibility: number;
+  windBearing: number;
+  windGust: number;
+  windKph: number;
+  windSpeed: number;
+}
---
+++ src/app/models/minutely.ts
@@ -0,0 +1,5 @@
+export interface Minutely {
+  precipIntensity: number;
+  precipProbability: number;
+  time: number;
+}
