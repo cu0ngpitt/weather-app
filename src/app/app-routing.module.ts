925e36d63c25aeac37abeaed4f224f4887157f1e
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2019-01-19 01:25:54 -0600

    Added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MainWeatherComponent } from './components/main-weather/main-weather.component';
+// import { MainWeatherComponent } from './components/main-weather/main-weather.component';
+// import { AppComponent } from './app.component';
+import { DailyForecastComponent } from './components/daily-forecast/daily-forecast.component';
+import { HourlyForecastComponent } from './components/hourly-forecast/hourly-forecast.component';
+import { MinutelyForecastComponent } from './components/minutely-forecast/minutely-forecast.component';
 
 const routes: Routes = [
-  { path: '', component: MainWeatherComponent }
+  // { path: 'current', component: MainWeatherComponent },
+  // { path: 'current', component: AppComponent },
+  { path: 'daily', component: DailyForecastComponent },
+  { path: 'hourly', component: HourlyForecastComponent },
+  { path: 'minutely', component: MinutelyForecastComponent },
+  // { path: '', redirectTo: '/current', pathMatch: 'full'}
 ];
 
 @NgModule({
